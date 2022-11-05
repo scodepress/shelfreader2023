e39086627d126787366805ecc9efd01118e93ef8
Scott <gildedpage@comcast.net>
2022-11-05 17:53:10 -0400

    replace migrations

---
+++ database/migrations/2022_06_27_131844_create_shelves_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShelvesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('scan_order');
+            $table->bigInteger('user_id');
+            $table->string('callnumber', 250);
+            $table->string('barcode', 250);
+            $table->text('title');
+            $table->bigInteger('shelf_position');
+            $table->bigInteger('correct_position');
+            $table->string('status', 45)->nullable();
+            $table->string('effective_shelving_order', 250)->nullable();
+            $table->string('effective_location_id', 250);
+            $table->string('effective_location_name', 250)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelves');
+    }
+}
